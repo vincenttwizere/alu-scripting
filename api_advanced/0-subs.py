a1ca7cc673504d492caca7932c4d8da44e765869
vincenttwizere <v.twizere@alustudent.com>
2023-07-15 10:56:48 +0000

    How many subs?

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,5 +1,6 @@
-
-n that queries the Reddit API and returns the number of subscribers
+#!/usr/bin/python3
+"""
+Function that queries the Reddit API and returns the number of subscribers
 (not active users, total subscribers) for a given subreddit.
 If an invalid subreddit is given, the function should return 0
 """
